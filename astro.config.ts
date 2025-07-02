6c7a06b96892859b8f8f11d0dfd923c5f96acc5b
KazuhiroIi <kazuhiro.ii@outlook.com>
2025-07-02 19:04:50 +0900

    デプロイ

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+//   site: 'https://kazuhiroii.github.io', // https://<あなたのGitHubのユーザー名>.github.io
+  base: 'ridai-astro-handson', // GitHubでのレポジトリ名
+});
