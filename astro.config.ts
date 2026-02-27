bcccc3cae04f3b3675e4300ae35693fdc3efbc59
KazuhiroIi <kazuhiro.ii@outlook.com>
2026-02-27 23:21:51 +0900

    chore: remove site and base configuration from astro.config.ts; add wrangler.jsonc for Cloudflare setup

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  // https://<あなたのGitHubのユーザー名>.github.io
-  site: 'https://KazuhiroIi.github.io',
-
-  // GitHubでのレポジトリ名
-  base: 'ridai-astro-handson',
-
   adapter: cloudflare()
 });
